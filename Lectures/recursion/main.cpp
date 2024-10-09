f50b5d44e635066b1a0b3267dee62674fc5d89ea
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-10-08 22:33:41 -0600

    thursday 1010

--- Lectures/recursion/main.cpp
+++ Lectures/recursion/main.cpp
@@ -5,23 +5,76 @@ using namespace std;
 int factorial(int);
 int iterFactorial(int);
 int fib(int);
+void reverseString(string &, int, int);
+void printStars(int);
 
 int main(int argc, char* argv[])
 {
-    int number;
+    // int number;
 
-    cout << "Enter the fibonacci number: ";
-    cin >> number;
+    // cout << "Enter the fibonacci number: ";
+    // cin >> number;
 
-    int fibNum = fib(number);
-    cout << "Fib number: " << number << " = " << fibNum << endl;
+    // int fibNum = fib(number);
+    // cout << "Fib number: " << number << " = " << fibNum << endl;
     
     // cout << "Enter a number for factorial: ";
     // cin >> number;
 
     // cout << number << "! = " << factorial(number) << endl;
     // cout << number << "! = " << iterFactorial(number) << endl;
+
+    // string inStr;
+
+    // cout << "Enter string to reverse: ";
+    // getline(cin, inStr);
+
+    // reverseString(inStr, 0, inStr.length()-1);
+    // cout << "reversed: " << inStr << endl;
+    int numStars;
+
+    cout << "Enter size of triangle: ";
+    cin >> numStars;
+
+    printStars(numStars);
+
     return 0;
+}
+
+//Prints a triangle of num stars
+// *
+// **
+// ***
+// ****
+// *****
+void printStars(int num)
+{
+    if (num < 1) return;
+
+    printStars(num - 1);
+
+    for(int i = 0; i < num; i++) cout << "*";
+    cout << endl;
+}
+
+void reverseString(string &str, int start, int end)
+{
+    if(start >= end)
+    {
+        return;
+    }
+    // char tmp = str[start];
+    // str[start] = str[end];
+    // str[end] = tmp;
+    swap(str[start], str[end]);
+
+    if(start == 0)
+    {
+        str[start] = toupper(str[start]);
+        str[end] = tolower(str[end]);
+    }
+
+    reverseString(str, start + 1, end - 1);
 }
 
 int fib(int n)
