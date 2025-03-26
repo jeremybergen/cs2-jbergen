4ac2ca2531a7527c35e65b1362edb88f0a89222a
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-03-26 13:51:23 -0600

    wednesday 0326

--- lecture/trees/main.cpp
+++ lecture/trees/main.cpp
@@ -28,9 +28,11 @@ class Tree
     Node<T1>* _root;
     Node<T1>* insertNode(T1, Node<T1>*);
     void inOrderPrint(Node<T1>*);
+    void deleteTree(Node<T1>*);
 
     public:
     Tree<T1>() {_root = nullptr;}
+    ~Tree();
     void insert(T1);
     void inOrder();
 };
@@ -42,9 +44,31 @@ int main(int argc, char* argv[])
     myTree.insert(42);
     myTree.insert(15);
     myTree.insert(90);
+    myTree.insert(366);
+    myTree.insert(365);
+    myTree.insert(16);
 
     myTree.inOrder();
     return 0;
+}
+
+template <class T1>
+void Tree<T1>::deleteTree(Node<T1>* root)
+{
+    if(root == nullptr)
+    {
+        return;
+    }
+    deleteTree(root->getLeft());
+    deleteTree(root->getRight());
+    cout << "root: " << root << endl;
+    delete root;
+}
+
+template <class T1>
+Tree<T1>::~Tree()
+{
+    deleteTree(_root);
 }
 
 template <class T1>
@@ -72,6 +96,11 @@ void Tree<T1>::inOrder()
 template <class T1>
 void Tree<T1>::insert(T1 data)
 {
+    // first value: insertNode(42, nullptr)
+    // second value: insertNode(15, 0x1230);
+    // third value: insertNode(90, 0x1230);
+    // fourth value: insertNode(366, 0x1230);
+    // fifth value: insertNode(365, 0x1230);
     _root = insertNode(data, _root);
 }
 
@@ -82,14 +111,24 @@ Node<T1>* Tree<T1>::insertNode(T1 data, Node<T1>* root)
     {
         Node<T1>* newNode = new Node<T1>;
         newNode->setData(data);
+        cout << "newNode: " << newNode << endl;
         return newNode;
     }
     if(root->getData() > data)
     {
+        // insertNode(15, nullptr)
+
+        // insertNode(365, nullptr)
         root->setLeft(insertNode(data, root->getLeft()));
     }
     else
     {
+        // insertNode(90, nullptr)
+        // insertNode(366, 0x5230)
+        // insertNode(366, nullptr)
+
+        // insertNode(365, 0x5230)
+        // insertNode(365, 0x1000)
         root->setRight(insertNode(data, root->getRight()));
     }
     return root;
