90ba6d0f776e31a236ee777c5f6a6ac43df4a2e1
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-03-27 13:51:31 -0600

    thursday 0327

--- lecture/trees/main.cpp
+++ lecture/trees/main.cpp
@@ -29,27 +29,115 @@ class Tree
     Node<T1>* insertNode(T1, Node<T1>*);
     void inOrderPrint(Node<T1>*);
     void deleteTree(Node<T1>*);
+    Node<T1>* removeNode(T1, Node<T1>*);
+    Node<T1>* maxVal(Node<T1>*);
 
     public:
     Tree<T1>() {_root = nullptr;}
     ~Tree();
     void insert(T1);
     void inOrder();
+    void remove(T1);
 };
 
 int main(int argc, char* argv[])
 {
     Tree<int> myTree;
 
+    // myTree.insert(42);
+    // myTree.insert(15);
+    // myTree.insert(90);
+    // myTree.insert(366);
+    // myTree.insert(365);
+    // myTree.insert(16);
+    myTree.insert(100);
     myTree.insert(42);
+    myTree.insert(200);
     myTree.insert(15);
-    myTree.insert(90);
-    myTree.insert(366);
-    myTree.insert(365);
-    myTree.insert(16);
+    myTree.insert(150);
+    myTree.insert(300);
+    myTree.insert(2);
+    myTree.insert(23);
+    myTree.insert(125);
+    myTree.insert(175);
+    myTree.insert(250);
+    myTree.insert(9000);
+    myTree.insert(160);
+
+    myTree.inOrder();
+
+    // cout << "Deleting 160" << endl;
+    // myTree.remove(160);
+    // cout << "Deleting 125" << endl;
+    // myTree.remove(125);
+    cout << "Deleting 42" << endl;
+    myTree.remove(42);
 
     myTree.inOrder();
     return 0;
+}
+
+template <class T1>
+Node<T1>* Tree<T1>::maxVal(Node<T1>* root)
+{
+    Node<T1>* largestNode = root;
+    while(largestNode->getRight() != nullptr)
+    {
+        largestNode = largestNode->getRight();
+    }
+    return largestNode;
+}
+
+template <class T1>
+Node<T1>* Tree<T1>::removeNode(T1 data, Node<T1>* root)
+{
+    if(root == nullptr) return nullptr;
+    if(root->getData() > data)
+    {
+        root->setLeft(removeNode(data, root->getLeft()));
+    }
+    else if(root->getData() < data)
+    {
+        root->setRight(removeNode(data, root->getRight()));
+    }
+    else
+    {
+        if(root->getLeft() == root->getRight())
+        {
+            cout << "Deleting: " << root << endl;
+            delete root;
+            return nullptr;
+        }
+        if(root->getLeft() != nullptr)
+        {
+            // return root->getLeft();
+            cout << "DEBUG: root->getLeft() != nullptr" << endl;
+            Node<T1>* leftNode = root->getLeft();
+            cout << "root: " << root << endl;
+            delete root;
+            return leftNode;
+        }
+        if(root->getRight() != nullptr)
+        {
+            // return root->getLeft();
+            cout << "DEBUG: root->getRight() != nullptr" << endl;
+            Node<T1>* rightNode = root->getRight();
+            cout << "root: " << root << endl;
+            delete root;
+            return rightNode;
+        }
+        Node<T1>* largest = maxVal(root->getLeft());
+        root->setData(largest->getData());
+        removeNode(largest->getData(), root->getLeft());
+    }
+
+    return root;
+}
+
+template <class T1>
+void Tree<T1>::remove(T1 data)
+{
+    _root = removeNode(data, _root);
 }
 
 template <class T1>
@@ -111,7 +199,7 @@ Node<T1>* Tree<T1>::insertNode(T1 data, Node<T1>* root)
     {
         Node<T1>* newNode = new Node<T1>;
         newNode->setData(data);
-        cout << "newNode: " << newNode << endl;
+        // cout << "newNode: " << newNode << endl;
         return newNode;
     }
     if(root->getData() > data)
