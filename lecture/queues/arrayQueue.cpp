da70a1d8cf56dfd56831def3d33b0df0ca558e7e
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-04-15 13:51:44 -0600

    tuesday 0415

--- lecture/queues/arrayQueue.cpp
+++ lecture/queues/arrayQueue.cpp
@@ -10,10 +10,55 @@ class Queue
     int _front, _back;
 
     public:
-
-}
+    Queue();
+    void enqueue(int);
+    int dequeue();
+    int front();
+    int size();
+    bool empty();
+    bool full();
+};
 
 int main(int argc, char* argv[])
 {
     return 0;
+}
+
+Queue::Queue()
+{
+    _front = -1;
+    _back = -1;
+}
+void Queue::enqueue(int data)
+{
+    if(empty())
+    {
+        _front++;
+        _back++;
+        _queueArr[_front] = data;
+        return;
+    }
+
+}
+int Queue::dequeue()
+{
+    return -1;
+}
+int Queue::front()
+{
+    return -1;
+}
+int Queue::size()
+{
+    return 0;
+}
+bool Queue::empty()
+{
+    if(_front == _back && _front == -1) return true;
+    return false;
+}
+bool Queue::full()
+{
+    if(_back == _maxQueue - 1 && _front == 0) return true;
+    return false;
 }
