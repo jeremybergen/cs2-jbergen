14cbe8ae414f4a4b265ef0fb20603fb2ef47ddd4
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-08-20 14:50:40 -0600

    wednesday 0820

--- lecture/review/arrays.cpp
+++ lecture/review/arrays.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+struct Person
+{
+    string fName;
+    string lName;
+    int age;
+};
+
 int main(int argc, char* argv[])
 {
+    int num1;
+    int* heapNum1 = new int;
+
+    num1 = 42;
+    *heapNum1 = 42;
+    
+    // Person people[3];
+
+    // for(int i = 0; i < 3; i++)
+    // {
+    //     cout << "Enter a name: ";
+    //     cin >> people[i].fName >> people[i].lName;
+
+    //     cout << "Enter an age: ";
+    //     cin >> people[i].age;
+    // }
+
+    // for(int i = 0; i < 3; i++)
+    // {
+    //     cout << "DEBUG: " << people[i].fName << ", " 
+    //          << people[i].lName << ", " 
+    //          << people[i].age << endl;
+    // }
+    delete heapNum1;
     return 0;
 }
--- lecture/review/structs.cpp
+++ lecture/review/structs.cpp
@@ -11,9 +11,18 @@ struct MyStruct
 int main(int argc, char* argv[])
 {
     MyStruct struct1;
+    MyStruct* ptr1;
+    MyStruct** ptr2;
 
-    struct1.num1 = 42;
-    struct1.f1 = 15.23;
+    ptr1 = &struct1;
+    ptr2 = &ptr1;
+
+    (**ptr2).num1 = 123;
+    // ptr1->f1 = 432.123;
+    (&struct1)->f1 = 12321.123;
+
+    // struct1.num1 = 42;
+    // struct1.f1 = 15.23;
 
     cout << "struct1.num1: " << struct1.num1 << endl;
     cout << "struct1.f1: " << struct1.f1 << endl;
