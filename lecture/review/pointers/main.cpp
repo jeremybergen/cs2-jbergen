834b2898c4969427459c152b813db25bb0406e3e
Jeremy Bergen <jbergen@coloradomesa.edu>
2026-01-21 13:51:10 -0700

    wednesday 0121

--- lecture/review/pointers/main.cpp
+++ lecture/review/pointers/main.cpp
@@ -2,12 +2,69 @@
 
 using namespace std;
 
+void someFun(int[], int);
+
 int main(int argc, char* argv[])
 {
-    int num1;
+    int numbers[10];
+    int* ptr1 = numbers;
 
-    num1 = 42;
+    
 
-    cout << "&num1: " << &num1 << endl;
+    // cout << "numbers: " << numbers << endl;
+    // cout << "&numbers[0]: " << &numbers[0] << endl;
+
+    // cout << "sizeof(numbers): " << sizeof(numbers) << endl;
+    // int* ptr1;
+
+    for(int i = 0; i < 10; i++)
+    {
+        numbers[i] = i+10;
+    }
+
+    someFun(ptr1, 10);
+
+    // ptr1 = numbers;
+    // for(int i = 1; i <= 10; i++)
+    // {
+    //     cout << "*ptr1: " << *ptr1 << endl;
+    //     ptr1 = ptr1 + 1;
+    // }
+
+    // ptr1 = &numbers[9];
+    // cout << "*ptr1: " << *ptr1 << endl;
+    // for(int i = 0; i < 10; i++)
+    // {
+    //     cout << "numbers[" << i << "]: " << &numbers[i] << endl;
+    // }
+
+    // int num1;
+    // int* ptr1;
+    // int** ptr2;
+
+    // num1 = 42;
+    // ptr1 = &num1;
+    // ptr2 = &ptr1;
+
+    // cout << "&num1: " << &num1 << endl;
+    // cout << "&ptr1: " << &ptr1 << endl;
+
+    // cout << "num1: " << num1 << endl;
+    // cout << "ptr1: " << ptr1 << endl;
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // cout << "ptr2: " << ptr2 << endl;
+    // cout << "&ptr2: " << &ptr2 << endl;
+    // cout << "the value in num1 from ptr2: " << **ptr2 << endl;
     return 0;
+}
+
+void someFun(int numbers[], int arrSize)
+{
+    // cout << "sizeof(numbers): " << sizeof(numbers) << endl;
+    for(int i = 0; i < arrSize; i++)
+    {
+        cout << "*(numbers + i): " << *(numbers + i) << endl;
+        cout << "numbers[" << i << "]: " << numbers[i] << endl;
+    }
 }
