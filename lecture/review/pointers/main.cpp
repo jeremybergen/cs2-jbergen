65de003ee36542ef44c5f9fbd624f960c3102fb8
Jeremy Bergen <jbergen@coloradomesa.edu>
2026-01-23 13:12:45 -0700

    Adding a comment to demonstrate commits

--- lecture/review/pointers/main.cpp
+++ lecture/review/pointers/main.cpp
@@ -6,23 +6,78 @@ void someFun(int[], int);
 
 int main(int argc, char* argv[])
 {
-    int numbers[10];
-    int* ptr1 = numbers;
-
-    
-
-    // cout << "numbers: " << numbers << endl;
-    // cout << "&numbers[0]: " << &numbers[0] << endl;
-
-    // cout << "sizeof(numbers): " << sizeof(numbers) << endl;
-    // int* ptr1;
+    int* numbers[10];
 
     for(int i = 0; i < 10; i++)
     {
-        numbers[i] = i+10;
+        numbers[i] = new int;
     }
 
-    someFun(ptr1, 10);
+    for(int i = 0; i < 10; i++)
+    {
+        *numbers[i] = i + 10;
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "numbers[" << i << "]: " << numbers[i] << endl;
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        delete numbers[i];
+    }
+
+    //This is a comment for the file
+    
+    // int num1, num2;
+    // int* heapNum1 = new int;
+    // int* heapNum2 = new int;
+
+    // cout << "&num1: " << &num1 << endl;
+    // cout << "&num2: " << &num2 << endl;
+
+    // cout << "&heapNum1: " << &heapNum1 << endl;
+    // cout << "heapNum1: " << heapNum1 << endl;
+    // cout << "&heapNum2: " << &heapNum2 << endl;
+    // cout << "heapNum2: " << heapNum2 << endl;
+
+    // *heapNum1 = 42;
+    // *heapNum2 = 24;
+
+    // delete heapNum1;
+    // delete heapNum2;
+
+    // long numbers[2000000];
+    // int somearray[1024][768][4];
+
+    // long* someimg = new long[2000000];
+    // long* someimg;
+    // someimg = new long[800000000];
+
+    // for(int i = 0; i < 800000000; i++)
+    // {
+    //     someimg[i] = i;
+    // }
+
+
+    // int numbers[10];
+    // int* ptr1 = numbers;
+
+    
+
+    // // cout << "numbers: " << numbers << endl;
+    // // cout << "&numbers[0]: " << &numbers[0] << endl;
+
+    // // cout << "sizeof(numbers): " << sizeof(numbers) << endl;
+    // // int* ptr1;
+
+    // for(int i = 0; i < 10; i++)
+    // {
+    //     numbers[i] = i+10;
+    // }
+
+    // someFun(ptr1, 10);
 
     // ptr1 = numbers;
     // for(int i = 1; i <= 10; i++)
@@ -56,6 +111,8 @@ int main(int argc, char* argv[])
     // cout << "ptr2: " << ptr2 << endl;
     // cout << "&ptr2: " << &ptr2 << endl;
     // cout << "the value in num1 from ptr2: " << **ptr2 << endl;
+
+    // delete[] someimg;
     return 0;
 }
 
