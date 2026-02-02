5c9e1ddd797b455521040c50df358c5b07dc0643
Jeremy Bergen <jbergen@coloradomesa.edu>
2026-02-02 13:42:37 -0700

    monday 0202

--- lecture/review/fileio/main.cpp
+++ lecture/review/fileio/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -14,10 +15,20 @@ using namespace std;
 
 // }
 
+struct MyStruct
+{
+    string fName;
+    string lName;
+    string title;
+    string color;
+};
+
 int main(int argc, char* argv[])
 {
     string inputLine;
     ifstream fin;
+    MyStruct* people[100];
+    int numPeople = 0;
 
     fin.open("test.csv");
 
@@ -26,11 +37,56 @@ int main(int argc, char* argv[])
     // while(fin >> inputLine)
     while(getline(fin, inputLine))
     {
-        cout << "DEBUG: " << inputLine << endl;
-        // for(int i = 0; i < inputLine.size(); i++)
-        // {
+        MyStruct* person = new MyStruct;
+        int tokenNum = 0;
+        string token;
+        istringstream iss;
+        iss.str(inputLine);
 
+        while(getline(iss, token, ','))
+        {
+            // cout << "DEBUG: " << token << endl;
+            // switch (tokenNum)
+            // {
+            // case 0:
+            //     person->fName = token;
+            //     break;
+            // case 1:
+            //     person->lName = token;
+            //     break;
+            // case 2:
+            //     person->title = token;
+            //     break;
+            // case 3:
+            //     person->color = token;
+            //     break;
+            
+            // default:
+            //     break;
+            // }
+            if(tokenNum == 0) person->fName = token;
+            if(tokenNum == 1) person->lName = token;
+            if(tokenNum == 2) person->title = token;
+            if(tokenNum == 3) person->color = token;
+            tokenNum++;
+        }
+        people[numPeople] = person;
+        // cout << "DEBUG: " << inputLine.substr() << endl;
+        // int lastCommaPos = 0;
+        // for(int i = 0; i <= inputLine.size(); i++)
+        // {
+        //     if(inputLine[i] == ',' || i == inputLine.size() - 1)
+        //     {
+        //         cout << "DEBUG: " << inputLine.substr(lastCommaPos, i - lastCommaPos) << endl;
+        //         lastCommaPos = i + 1;
+        //     }
         // }
+        numPeople++;
+    }
+
+    for(int i = 0; i < numPeople; i++)
+    {
+        cout << "Favorite Color: " << people[i]->color << endl;
     }
 
     fin.close();
