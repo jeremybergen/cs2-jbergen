458213a6dc2cfc3d1c561a8771b259ec3d2f9e14
Jeremy Bergen <jbergen@coloradomesa.edu>
2026-02-04 13:50:59 -0700

    wednesday 0204

---
+++ lecture/classes/lib/marker.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Marker
+{
+    private:
+        string _color;
+        float _inklevel;
+        string _brand;
+        bool _cap;
+        int* ptr1;
+    public:
+        // Marker();
+        // Marker(string color, float inklevel, string brand, bool cap);
+        Marker(string color = "Black", float inklevel = 1.0, string brand = "Expo", bool cap = true);
+        ~Marker();
+        //Getter
+        string getColor();
+        float getInklevel();
+        string getBrand();
+
+        //Setter
+        void setColor(string color);
+        void setInklevel(float inklevel);
+
+        void toggleCap();
+};
+
+Marker::Marker(string color, float inklevel, string brand, bool cap)
+{
+    _color = color;
+    _inklevel = inklevel;
+    _brand = brand;
+    _cap = cap;
+    // _color = "Black";
+    // _inklevel = 1.0;
+    // _cap = true;
+    // _brand = "Expo";
+}
+
+string Marker::getBrand()
+{
+    return _brand;
+}
+
+string Marker::getColor()
+{
+    return _color;
+}
+float Marker::getInklevel()
+{
+    return _inklevel;
+}
+
+//Setter
+void Marker::setColor(string color)
+{
+    _color = color;
+}
+void Marker::setInklevel(float inklevel)
+{
+    // _inklevel = inklevel;
+    if(inklevel < 0 || inklevel > 1)
+    {
+        cerr << "inklevel must be between 0 and 1" << endl;
+    }
+    else
+    {
+        _inklevel = inklevel;
+    }
+}
+
+void Marker::toggleCap()
+{
+    if(_cap) _cap = false;
+    else _cap = true;
+}
--- lecture/classes/main.cpp
+++ lecture/classes/main.cpp
@@ -1,59 +1,46 @@
 #include <iostream>
+#include "lib/marker.h"
 
 using namespace std;
 
-class Marker
-{
-    private:
-        string _color;
-        float _inklevel;
-        string _brand;
-        bool _cap;
-    public:
-        //Getter
-        string getColor()
-        {
-            return _color;
-        }
-        float getInklevel()
-        {
-            return _inklevel;
-        }
-
-        //Setter
-        void setColor(string color)
-        {
-            _color = color;
-        }
-        void setInklevel(float inklevel)
-        {
-            // _inklevel = inklevel;
-            if(inklevel < 0 || inklevel > 1)
-            {
-                cerr << "inklevel must be between 0 and 1" << endl;
-            }
-            else
-            {
-                _inklevel = inklevel;
-            }
-        }
-
-        void toggleCap()
-        {
-            if(_cap) _cap = false;
-            else _cap = true;
-        }
-};
+void sayHello();
+void sayGoodbye();
 
 int main(int argc, char* argv[])
 {
-    Marker marker1;
+    // sayHello();
+    // string* str1 = new string("Jeremy");
+    // int* num1 = new int(42);
+    Marker* marker1 = new Marker("Red", .75, "Sharpie", false);
+    Marker* marker2 = new Marker("Black", .5);
+    Marker* marker3 = new Marker();
 
-    marker1.setColor("Black");
-    marker1.setInklevel(.75);
+    // Marker marker1;
 
-    cout << "marker1: color: " << marker1.getColor() 
-         << ", ink level: " << marker1.getInklevel() << endl;
+    // marker1->setColor("Black");
+    // marker1->setInklevel(.75);
 
+    marker1->toggleCap();
+    cout << "marker1: color: " << marker1->getColor() 
+         << ", ink level: " << marker1->getInklevel() 
+         << ", brand: " << marker1->getBrand() << endl;
+
+    cout << "marker2: color: " << marker2->getColor() 
+         << ", ink level: " << marker2->getInklevel() 
+         << ", brand: " << marker2->getBrand() << endl;
+
+    delete marker1;
     return 0;
+}
+
+
+void sayGoodbye()
+{
+    cout << "Goodbye" << endl;
+    sayHello();
+}
+void sayHello()
+{
+    cout << "Hello" << endl;
+    sayGoodbye();
 }
