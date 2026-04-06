ae7e061ec2a4b5740af622e73276763e385ac373
Jeremy Bergen <jbergen@coloradomesa.edu>
2026-04-06 13:17:30 -0600

    thursday 0402

--- lecture/trees/main.cpp
+++ lecture/trees/main.cpp
@@ -2,79 +2,103 @@
 
 using namespace std;
 
+template <class T1>
 class Node
 {
     private:
-    int _data;
-    Node* _left;
-    Node* _right;
+    T1 _data;
+    Node<T1>* _left;
+    Node<T1>* _right;
 
     public:
-    Node(int data = 0, Node* left = nullptr, Node* right = nullptr)
+    Node(T1 data, Node<T1>* left = nullptr, Node<T1>* right = nullptr)
     {
         _data = data;
         _left = left;
         _right = right;
     }
-    int getData() { return _data; }
-    Node* getLeft() { return _left; }
-    Node* getRight() { return _right; }
-    void setLeft(Node* left) { _left = left; }
-    void setRight(Node* right) { _right = right; }
+    T1 getData() { return _data; }
+    Node<T1>* getLeft() { return _left; }
+    Node<T1>* getRight() { return _right; }
+    void setLeft(Node<T1>* left) { _left = left; }
+    void setRight(Node<T1>* right) { _right = right; }
+    void setData(T1 data) { _data = data; }
 };
 
+template <class T1>
 class Tree
 {
     private:
-    Node* _root;
-    Node* insert(Node* newRoot, int data);
-    void print(Node* root);
-    Node* remove(Node* root, int data);
-    Node* maxVal(Node* root);
+    Node<T1>* _root;
+    Node<T1>* insert(Node<T1>* newRoot, T1 data);
+    void print(Node<T1>* root);
+    Node<T1>* remove(Node<T1>* root, T1 data);
+    Node<T1>* maxVal(Node<T1>* root);
 
     public:
     Tree() { _root = nullptr; }
-    void insert(int data);
-    Node* getRoot() { return _root; }
+    void insert(T1 data);
+    Node<T1>* getRoot() { return _root; }
     void print();
-    void remove(int data);
+    void remove(T1 data);
+};
+
+class MyClass
+{
+    private:
+    int _data;
+
+    public:
+    MyClass(int data) : _data(data) {}
+    bool operator<(const MyClass rhs)
+    {
+        return this->_data < rhs._data;
+    }
+
+    friend ostream& operator<<(ostream& os, const MyClass mc1)
+    {
+        os << mc1._data;
+        return os;
+    }
 };
 
 int main(int argc, char* argv[])
 {
-    Tree myTree;
-    int toBeDeleted;
+    Tree<MyClass> myTree;
+    string toBeDeleted;
 
-    myTree.insert(42);
-    myTree.insert(15);
-    myTree.insert(23);
-    myTree.insert(-2);
+    myTree.insert(MyClass(42));
+    myTree.insert(MyClass(15));
+    myTree.insert(MyClass(23));
+    myTree.insert(MyClass(-2));
     // myTree.insert(8);
-    myTree.insert(9000);
-    myTree.insert(69);
-    myTree.insert(100);
-    myTree.insert(10000);
+    // myTree.insert("9000");
+    // myTree.insert("69");
+    // myTree.insert("100");
+    // myTree.insert("10000");
 
     myTree.print();
     cout << endl;
 
-    cout << "Enter number to delete: ";
-    cin >> toBeDeleted;
-    myTree.remove(toBeDeleted);
-    // cout << "after remove" << endl;
+    // cout << "Enter number to delete: ";
+    // cin >> toBeDeleted;
+    // myTree.remove(toBeDeleted);
+    // // cout << "after remove" << endl;
 
-    myTree.print();
-    cout << endl;
+    // myTree.print();
+    // cout << endl;
 
     return 0;
 }
 
-void Tree::remove(int data)
+template <class T1>
+void Tree<T1>::remove(T1 data)
 {
     _root = remove(_root, data);
 }
 
-Node* Tree::remove(Node* root, int data)
+template <class T1>
+Node<T1>* Tree<T1>::remove(Node<T1>* root, T1 data)
 {
     if(root == nullptr) return root;
     if(data < root->getData())
@@ -90,13 +114,14 @@ Node* Tree::remove(Node* root, int data)
         //The data exists at this "root"
         if(root->getLeft() == nullptr)
         {
-            Node* toReturn = root->getRight();
+            // cout << "DEBUG: inside root->getLeft() == nullptr" << endl;
+            Node<T1>* toReturn = root->getRight();
             delete root;
             return toReturn;
         }
         else if(root->getRight() == nullptr)
         {
-            Node* toReturn = root->getLeft();
+            Node<T1>* toReturn = root->getLeft();
             delete root;
             return toReturn;
         }
@@ -104,39 +129,46 @@ Node* Tree::remove(Node* root, int data)
         {
             //Both left and right has a node
             // cout << "DEBUG: " << root << endl;
-            Node* largestNode = maxVal(root->getLeft());
+            Node<T1>* largestNode = maxVal(root->getLeft());
+            // cout << "DEBUG: largestNode->getData(): " << largestNode->getData() << endl;
 
-            largestNode->setRight(root->getRight());
-            largestNode->setLeft(root->getLeft());
+            root->setData(largestNode->getData());
+            root->setLeft(remove(root->getLeft(), largestNode->getData()));
+            // largestNode->setRight(root->getRight());
+            // largestNode->setLeft(root->getLeft());
             // if(root == _root) _root = largestNode; THE WRONG WAY!!!!
-            delete root;
+            // delete root;
             // cout << "After updating node" << endl;
-            return largestNode;
+            // return largestNode;
+            return root;
         }
     }
     return root;
 }
 
-Node* Tree::maxVal(Node* root)
+template <class T1>
+Node<T1>* Tree<T1>::maxVal(Node<T1>* root)
 {
-    Node* curNode = root;
-    Node* parent = root;
+    Node<T1>* curNode = root;
+    // Node<T1>* parent = root;
     while(curNode->getRight() != nullptr)
     {
-        parent = curNode;
+        // parent = curNode;
         curNode = curNode->getRight();
     }
-    parent->setRight(nullptr);
+    // parent->setRight(nullptr);
     return curNode;
 }
 
-void Tree::print()
+template <class T1>
+void Tree<T1>::print()
 {
-    // cout << "_root: " << _root << endl;
+    // cout << "_root: " << _root->getData() << endl;
     print(_root);
 }
 
-void Tree::print(Node* root)
+template <class T1>
+void Tree<T1>::print(Node<T1>* root)
 {
     //in order traversal
     if(root == nullptr) return;
@@ -146,20 +178,22 @@ void Tree::print(Node* root)
     print(root->getRight());
 }
 
-void Tree::insert(int data)
+template <class T1>
+void Tree<T1>::insert(T1 data)
 {
     // cout << "Inserting " << data << endl;
     _root = insert(_root, data);
     // cout << "_root: " << _root << endl;
 }
 
-Node* Tree::insert(Node* newRoot, int data)
+template <class T1>
+Node<T1>* Tree<T1>::insert(Node<T1>* newRoot, T1 data)
 {
     
     if(newRoot == nullptr)
     {
         // cout << "newRoot is a nullptr" << endl;
-        Node* newNode = new Node(data);
+        Node<T1>* newNode = new Node(data);
         // newRoot = newNode;
         return newNode;
     }
@@ -175,9 +209,4 @@ Node* Tree::insert(Node* newRoot, int data)
         newRoot->setRight(insert(newRoot->getRight(), data));
     }
     return newRoot;
-}
-
-void print()
-{
-    
 }
